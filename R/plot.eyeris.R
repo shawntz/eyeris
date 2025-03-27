91a7b0b3bbf97ca180ae6ca02e5c0d4d41898224
shawn schwartz <sts@hey.com>
2025-03-27 15:29:23 -0700

    BF: resolve invalid modification of user's options without safety reset mechanism #133

--- R/plot.eyeris.R
+++ R/plot.eyeris.R
@@ -75,6 +75,10 @@
 plot.eyeris <- function(x, ..., steps = NULL, num_previews = NULL,
                         preview_duration = NULL, preview_window = NULL,
                         seed = NULL, block = 1, plot_distributions = TRUE) {
+  # safely handle user's current options
+  oldpar <- par(no.readonly = TRUE)
+  on.exit(par(oldpar))
+
   # tests
   tryCatch(
     {
@@ -422,6 +426,10 @@ robust_plot <- function(x, ...) {
 }
 
 plot_pupil_distribution <- function(data, color, main, xlab) {
+  # safely handle user's current options
+  oldpar <- par(no.readonly = TRUE)
+  on.exit(par(oldpar))
+
   par(mfrow = c(1, 1), oma = c(0, 0, 0, 0))
 
   hist(
