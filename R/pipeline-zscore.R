9468f05396028566468007dc6b07d775df1a2e4f
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2025-04-24 04:45:32 -0700

    DOC: simplify example code in `pipeline-zscore.R`

--- R/pipeline-zscore.R
+++ R/pipeline-zscore.R
@@ -56,13 +56,10 @@
 #' part of the full eyeris glassbox preprocessing pipeline.
 #'
 #' @examples
-#' system.file("extdata", "memory.asc", package = "eyeris") |>
-#'   eyeris::load_asc() |>
-#'   eyeris::deblink(extend = 50) |>
-#'   eyeris::detransient() |>
-#'   eyeris::interpolate() |>
-#'   eyeris::lpfilt(plot_freqz = TRUE) |>
-#'   eyeris::zscore() |>
+#' demo_data <- eyelink_asc_demo_dataset()
+#'
+#' demo_data |>
+#'   eyeris::glassbox(zscore = TRUE) |> # set to FALSE to skip (not recommended)
 #'   plot(seed = 0)
 #'
 #' @export
