fc37244d9bb2f174b950b17cb7663ec3616634a3
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2025-04-24 04:39:47 -0700

    DOC: add more notes and details to `pipeline-interpolate.R` for clarity

--- R/pipeline-interpolate.R
+++ R/pipeline-interpolate.R
@@ -8,13 +8,30 @@
 #' values on either end, respectively, using the "rule = 2" argument in the
 #' `approx()` function.
 #'
+#' @note
+#' This function is part of the `glassbox()` preprocessing pipeline and is not
+#' intended for direct use in most cases. Use `glassbox(interpolate = TRUE)`.
+#'
+#' Advanced users may call it directly if needed.
+#'
+#' @details
+#' This function is automatically called by `glassbox()` by default. Use
+#' `glassbox(interpolate = FALSE)` to disable this step as needed.
+#'
+#' Users should prefer using `glassbox()` rather than invoking this function
+#' directly unless they have a specific reason to customize the pipeline
+#' manually.
+#'
 #' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
 #' @param verbose A flag to indicate whether to print detailed logging messages.
-#' Defaults to `TRUE`. Set to `False` to suppress messages about the current
+#' Defaults to `TRUE`. Set to `FALSE` to suppress messages about the current
 #' processing step and run silently.
 #'
 #' @return An `eyeris` object with a new column in `timeseries`:
 #' `pupil_raw_{...}_interpolate`.
+#'
+#' @seealso [eyeris::glassbox()] for the recommended way to run this step as
+#' part of the full eyeris glassbox preprocessing pipeline.
 #'
 #' @examples
 #' system.file("extdata", "memory.asc", package = "eyeris") |>
