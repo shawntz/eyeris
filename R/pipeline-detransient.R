1f30f8d7d939a5ab7052b3e3af72e13af5aa9dde
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2025-04-24 04:43:56 -0700

    DOC: simplify example code in `pipeline-detransient.R`

--- R/pipeline-detransient.R
+++ R/pipeline-detransient.R
@@ -86,10 +86,12 @@
 #' part of the full eyeris glassbox preprocessing pipeline.
 #'
 #' @examples
-#' system.file("extdata", "memory.asc", package = "eyeris") |>
-#'   eyeris::load_asc() |>
-#'   eyeris::deblink(extend = 50) |>
-#'   eyeris::detransient() |>
+#' demo_data <- eyelink_asc_demo_dataset()
+#'
+#' demo_data |>
+#'   eyeris::glassbox(
+#'     detransient = list(n = 16) # set to FALSE to skip step (not recommended)
+#'   ) |>
 #'   plot(seed = 0)
 #'
 #' @export
