cdd9cbd834bf767b7386335c4af2edb1e2711bfc
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2025-06-06 12:01:35 -0700

    CHORE: add missing (new) `time_secs` global var to `zzz.R`

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 utils::globalVariables(c(
-  "time_orig", "pupil", "blink", "blink.lag", "time", "blink.start",
-  "blink.lead", "blink.end", "pupil_deblink", "event", "ps", "xp", "yp",
-  "pupil_raw", "type", "block", "timebin", "step", "eyeris", "pupil_size",
-  "matched_event", "matching_pattern", "s", "e"
+  "time_orig", "time_secs", "pupil", "blink", "blink.lag", "time",
+  "blink.start", "blink.lead", "blink.end", "pupil_deblink", "event",
+  "ps", "xp", "yp", "pupil_raw", "type", "block", "timebin", "step", "eyeris",
+  "pupil_size", "matched_event", "matching_pattern", "s", "e"
 ))
