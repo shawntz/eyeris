713cf555ec18057cb3cf6146724cad6e9c937fdf
Shawn Schwartz <shawn@shawnschwartz.com>
2025-07-21 19:45:30 -0700

    CHORE: Update utils-progress_bar.R with air

--- R/utils-progress_bar.R
+++ R/utils-progress_bar.R
@@ -13,12 +13,14 @@
 #' @return A progress bar object from the progress package
 #'
 #' @keywords internal
-progress_bar <- function(total, msg = "Processing", width = 80,
-                         show_percent = TRUE, show_eta = TRUE,
-                         clear = FALSE) {
+progress_bar <- function(total, msg = "Processing", width = 80, show_percent = TRUE, show_eta = TRUE, clear = FALSE) {
   format_str <- c(msg, "[:bar]")
-  if (show_percent) format_str <- c(format_str, ":percent")
-  if (show_eta) format_str <- c(format_str, "eta: :eta")
+  if (show_percent) {
+    format_str <- c(format_str, ":percent")
+  }
+  if (show_eta) {
+    format_str <- c(format_str, "eta: :eta")
+  }
   format_str <- paste(format_str, collapse = " ")
 
   progress::progress_bar$new(
