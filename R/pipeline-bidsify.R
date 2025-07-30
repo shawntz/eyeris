56c35d6d291f0f8c22d0b4d265ef726484e58619
shawn schwartz <shawn.t.schwartz@gmail.com>
2025-07-29 23:26:21 -0700

    Update pipeline-bidsify.R

--- R/pipeline-bidsify.R
+++ R/pipeline-bidsify.R
@@ -2621,6 +2621,12 @@ run_bidsify <- function(
     )
 
     render_report(report_output)
+
+    cleanup_source_figures_post_render(
+      report_path = report_path,
+      eye_suffix = eye_suffix,
+      verbose = verbose
+    )
   }
 }
 
