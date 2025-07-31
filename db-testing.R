668d1f3d7a8506036c03f67d8b56891892a358b0
Shawn Schwartz <shawn@shawnschwartz.com>
2025-07-31 00:41:42 -0700

    Update db-testing.R

--- db-testing.R
+++ db-testing.R
@@ -1,35 +1,34 @@
 con <- eyeris_db_connect(
-  file.path("~/Downloads", "newdbbaseline")
+  file.path("~/Downloads", "debugging")
 )
 
 tables <- eyeris_db_list_tables(con)
 tables
 
-data <- eyeris_db_read(con, data_type = "blinks", subject = "001")
+data <- eyeris_db_read(con, data_type = "blinks", subject = "056")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "confounds_events", subject = "001", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "confounds_events", subject = "056", epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "confounds_summary", subject = "001", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "confounds_summary", subject = "056", epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epoch_summary", subject = "001")
+data <- eyeris_db_read(con, data_type = "epoch_summary", subject = "056")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epochs", subject = "001", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "epochs", subject = "056", epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "events", subject = "001")
+data <- eyeris_db_read(con, data_type = "events", subject = "056")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "run_confounds", subject = "001")
+data <- eyeris_db_read(con, data_type = "run_confounds", subject = "056")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "timeseries", subject = "001")
+data <- eyeris_db_read(con, data_type = "timeseries", subject = "056")
 data |> View()
 
-# TODO: test baselining condition
 # TODO: prevent duplicate entries (drop all entries for given subid beforehand)
 
 
