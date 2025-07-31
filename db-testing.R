cbea645c1011df9d3c477630b1800c8d57941795
Shawn Schwartz <shawn@shawnschwartz.com>
2025-07-30 21:04:33 -0700

    Update db-testing.R

--- db-testing.R
+++ db-testing.R
@@ -1,33 +1,34 @@
 con <- eyeris_db_connect(
-  file.path("~/Downloads", "testdb")
+  file.path("~/Downloads", "newdbbaseline")
 )
 
 tables <- eyeris_db_list_tables(con)
 tables
 
-data <- eyeris_db_read(con, data_type = "blinks", subject = "002")
+data <- eyeris_db_read(con, data_type = "blinks", subject = "001")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "confounds", subject = "002")
+data <- eyeris_db_read(con, data_type = "confounds_events", subject = "001", epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epoch_summary", subject = "002")
+data <- eyeris_db_read(con, data_type = "confounds_summary", subject = "001", epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epoch_timeseries", subject = "002", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "epoch_summary", subject = "001")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epochs", subject = "002", epoch_label = "prepostprobe")
-data |> View() # TODO: remove (duplicate table entry of epoch_timeseries)
-
-data <- eyeris_db_read(con, data_type = "events", subject = "002")
+data <- eyeris_db_read(con, data_type = "epochs", subject = "001", epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "timeseries", subject = "002")
+data <- eyeris_db_read(con, data_type = "events", subject = "001")
 data |> View()
 
-# TODO: missing sub-002_ses-01_task-assocret_run-01_desc-confounds_summary_PROBE_START_22_epoch-prepostprobe.csv
-# TODO: missing sub-002_ses-01_task-assocret_run-01_desc-confounds_events_PROBE_START_22_epoch-prepostprobe.csv
+data <- eyeris_db_read(con, data_type = "run_confounds", subject = "001")
+data |> View()
+
+data <- eyeris_db_read(con, data_type = "timeseries", subject = "001")
+data |> View()
+
 # TODO: test baselining condition
 # TODO: prevent duplicate entries (drop all entries for given subid beforehand)
 
