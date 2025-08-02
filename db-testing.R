77dc698ab5770ff1e885933ee473b7da0f40cc29
Shawn Schwartz <shawn@shawnschwartz.com>
2025-08-01 22:42:32 -0700

    Update db-testing.R

--- db-testing.R
+++ db-testing.R
@@ -1,5 +1,5 @@
 con <- eyeris_db_connect(
-
+  file.path("~/Downloads", "debugging")
 )
 
 eyeris::eyeris_extract_data(file.path("~/Downloads", "debugging")) -> pop
@@ -8,28 +8,28 @@ pop |> View()
 tables <- eyeris_db_list_tables(con)
 tables
 
-data <- eyeris_db_read(con, data_type = "blinks", subject = "056")
+data <- eyeris_db_read(con, data_type = "blinks", subject = "059")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "confounds_events", subject = "056", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "confounds_events", subject = "059")#, epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "confounds_summary", subject = "056", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "confounds_summary", subject = "059")#, epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epoch_summary", subject = "056")
+data <- eyeris_db_read(con, data_type = "epoch_summary", subject = "059")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "epochs", subject = "056", epoch_label = "prepostprobe")
+data <- eyeris_db_read(con, data_type = "epochs", subject = "059")#, epoch_label = "prepostprobe")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "events", subject = "056")
+data <- eyeris_db_read(con, data_type = "events", subject = "059")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "run_confounds", subject = "056")
+data <- eyeris_db_read(con, data_type = "run_confounds", subject = "059")
 data |> View()
 
-data <- eyeris_db_read(con, data_type = "timeseries", subject = "056")
+data <- eyeris_db_read(con, data_type = "timeseries", subject = "059")
 data |> View()
 
 
