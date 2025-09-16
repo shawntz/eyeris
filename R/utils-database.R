f9378830cad9ce717eb8ca3e256c074741097599
Shawn Schwartz <shawn@shawnschwartz.com>
2025-09-16 14:24:46 -0700

    Update R/utils-database.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/utils-database.R
+++ R/utils-database.R
@@ -146,7 +146,7 @@ create_table_name <- function(
   if (!check_duckdb()) {
     log_error(
       "DuckDB is required for this feature. See installation instructions above.",
-      verbose = TRUE  
+      verbose = TRUE
     )
   }
 
