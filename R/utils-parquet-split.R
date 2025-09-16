e396a259f48c2bdf7351e03bbd2c578433c6147c
Shawn Schwartz <shawn@shawnschwartz.com>
2025-09-16 14:25:25 -0700

    Update R/utils-parquet-split.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/utils-parquet-split.R
+++ R/utils-parquet-split.R
@@ -84,7 +84,7 @@ eyeris_db_to_parquet <- function(
   if (!check_duckdb()) {
     log_error(
       "DuckDB is required for this feature. See installation instructions above.",
-      verbose = TRUE  
+      verbose = TRUE
     )
   }
 
