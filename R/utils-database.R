27655d740291f712a01f12e82b08d1b4c0d706a1
Shawn Schwartz <shawn@shawnschwartz.com>
2025-09-16 14:27:39 -0700

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/utils-database.R
+++ R/utils-database.R
@@ -783,7 +783,7 @@ eyeris_db_collect <- function(
   if (!check_duckdb()) {
     log_error(
       "DuckDB is required for this feature. See installation instructions above.",
-      verbose = TRUE  
+      verbose = TRUE
     )
   }
 
